95a1b572f273f595bc5a429e2e0e38f8ff6a40a4
Timur Simonov <um4d414@ya.ru>
2025-03-02 18:31:49 +0300

    Initial commit

---
+++ src/main/java/ru/umd/intershop/IntershopApplication.java
@@ -0,0 +1,13 @@
+package ru.umd.intershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/umd/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.umd.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
